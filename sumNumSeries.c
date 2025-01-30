7dbb82c853ef920c459b9d960f9caaa22537c47f
jirat8692 <47243@kalasinpit.ac.th>
2025-01-30 13:50:23 +0700

    Update sumNumSeries.c

--- sumNumSeries.c
+++ sumNumSeries.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
-int sum_of_numbers(int n) {
-    return n * (n + 1) / 2;
-}
-
-int main() {
-    int n;
-
-    printf("Enter a number n : ");
-    scanf("%d", &n);
-
-    printf("The sum of numbers from 1 to %d is: %d\n", n, sum_of_numbers(n));
-
-    return ();
+int main (){
+    int i = 1,n,sum = 0;
+    scanf("%d" ,&n);
+    while(i <=n) {
+          sum = sum + i;
+          i = i+ 1;
+    }
+    printf("%d",sum);
 }
